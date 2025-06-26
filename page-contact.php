bec523ce5e645b83cd9a177646881b58f8adc1f8
zackmorgs <zackmorgenthaler@gmail.com>
2025-06-26 09:00:53 -0500

    Enhance contact and merch pages with dynamic content and new SVG assets

--- page-contact.php
+++ page-contact.php
@@ -1,11 +1,23 @@
 <?php get_header(); ?>
 <main>
-  <?php 
-    get_template_part(
-      slug: 'template-parts/hero', 
-      name: null, 
-      args: ['title' => 'Contact']); 
-    ?>
-
+  <?php
+  get_template_part(
+    slug: 'template-parts/hero',
+    name: null,
+    args: ['title' => 'Contact']
+  );
+  ?>
+  <section class="content">
+    <div class="content-float">
+      <?php
+      if (have_posts()):
+        while (have_posts()):
+          the_post();
+          the_content(); // This is the important line!
+        endwhile;
+      endif;
+      ?>
+    </div>
+  </section>
 </main>
 <?php get_footer(); ?>
--- page-merch.php
+++ page-merch.php
@@ -7,6 +7,14 @@
     args: ['title' => 'Merch']
   );
   ?>
+  <section id="content_big_cartel" class="content">
+    <div class="content-float">
+      <p class="text-lg">Check out our merch store for the latest Still Stayer gear!</p>
+      <a href="https://stillstayer.bigcartel.com/" class="btn btn-big-cartel" target="_blank">
+        <img src="<?php echo get_template_directory_uri(); ?>/svg/big_cartel.svg" alt="Merch Store Linkj" class="icon icon-big-cartel" />
+      </a>
+    </div>
+  </section>
   <section class="content">
     <div class="content-float">
       <?php
