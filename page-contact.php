521159dbc12290b6dbf26d9de566b9f57e54b849
zackmorgs <zackmorgenthaler@gmail.com>
2025-06-27 14:48:45 -0500

    Enhance styles and update contact page content for improved user experience

--- page-contact.php
+++ page-contact.php
@@ -9,14 +9,16 @@
   ?>
   <section id="content_contact" class="content">
     <div class="content-float">
-      <?php
-      if (have_posts()):
-        while (have_posts()):
-          the_post();
-          the_content(); // This is the important line!
-        endwhile;
-      endif;
-      ?>
+      <h2>Email Us:</h2>
+      <p>For all inquries, please shoot us an email!</p>
+      <a href="mailto:stillstayer@gmail.com" >
+        stillstayer@gmail.com
+      </a>
+      <br/>
+      <br/>
+      <a href="mailto:stillstayer@gmail.com" class="btn btn-email">
+        <img src="<?php echo get_template_directory_uri(); ?>/svg/mail.svg" alt="Email Icon" class="icon-mail" />
+      </a>
     </div>
   </section>
 </main>
--- readme.md
+++ readme.md
@@ -5,15 +5,19 @@ My go at a wordpress theme for the band [Still Stayer](https://facebook.com/stil
 1. Download the repo as a .zip above.
 2. Place it in the themes folder
     - `/wp-content/themes/`
-3. Create the following pages using wordpress. Just give them a title - add content after.
+3. Create the following pages using wordpress. Just give them a title for now. 
     - Listen
+        - Not content editable.
     - Shows
     - Merch
     - Gallery
     - Blog
     - Contact
+        - Not content editable.
+
 4. Settings > Reading 
     - Homepage: "Home"
     - Posts Page: "Blog"
-5. Done ✔️
+5. Create your blog posts and add content to pages. (Unless content is not editable on page)
+6. Done ✔️
 
