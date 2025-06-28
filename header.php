c763eef0e01b74881d66122150650aff735cf055
zackmorgs <zackmorgenthaler@gmail.com>
2025-06-28 04:44:41 -0500

    installed & configured bundling/minification for js/css

--- header.php
+++ header.php
@@ -11,7 +11,7 @@
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=GFS+Didot&display=swap" rel="stylesheet">
-  <link href="<?php echo get_template_directory_uri(); ?>/style.css" rel="stylesheet" />
+  <link href="<?php echo get_template_directory_uri(); ?>/style.min.css" rel="stylesheet" />
 
   <link rel="apple-touch-icon" sizes="180x180"
     href="<?php echo get_template_directory_uri(); ?>/img/apple-touch-icon.png">
